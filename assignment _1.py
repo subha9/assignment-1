cc284c2626eaf8ea4d8bcae1f0c89b0a076a02ec
subha9 <40471824+subha9@users.noreply.github.com>
2018-06-23 13:04:03 -0500

    Add files via upload

---
+++ assignment _1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 12:02:42 2018
+
+@author: Rao
+"""
+
+fname= input(" Please enter your first name :")
+lname = input("Please enter your lastname :")
+print (lname + " " + fname)
+
+
